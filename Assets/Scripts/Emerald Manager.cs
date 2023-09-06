49c7eee1238c414de9cab8162774b59a11fd309c
BiozW <jirayusuk@gmail.com>
2023-09-06 10:05:14 +0700

    ADD: Infinity function in Emerald Manager

--- Assets/Scripts/Emerald Manager.cs
+++ Assets/Scripts/Emerald Manager.cs
@@ -10,21 +10,30 @@ namespace Minecraft.InventorySystem
     {
         // Start is called before the first frame update
         public TMPro.TMP_Text emeraldText;
+        public bool toggleInfinite = false;
         public int myemerald = 64;
 
         // Update is called once per frame
         void Update()
         {
+            InfiniteEmerald();
             EmeraldUpdate();
+        }
+
+        public void InfiniteEmerald()
+        {
+            if (toggleInfinite == true)
+            {
+                if (myemerald <= 3)
+                {
+                    myemerald = 64;
+                }
+            }
+            
         }
 
         public void EmeraldUpdate()
         {
-            if (myemerald == 0)
-            {
-                
-            }
-            else
             emeraldText.text = myemerald.ToString();
         }
     }
